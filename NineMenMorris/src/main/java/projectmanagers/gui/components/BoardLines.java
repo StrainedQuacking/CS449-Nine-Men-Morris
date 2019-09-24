b5772962bc2943548d40834b8ebbe3f05c5a737e
Sam Feye <samfeye@gmail.com>
2019-09-23 19:33:48 -0500

    Added paintComponent methods and mouseListeners to components.

--- NineMenMorris/src/main/java/projectmanagers/gui/components/BoardLines.java
+++ NineMenMorris/src/main/java/projectmanagers/gui/components/BoardLines.java
@@ -1,4 +1,43 @@
 package main.java.projectmanagers.gui.components;
 
-public class BoardLines {
+import javax.swing.*;
+import java.awt.*;
+
+public class BoardLines extends JComponent {
+    private static final int HORIZONTAL = 1;
+    private static final int VERTICAL = 2;
+    private static final int LEFT_HORIZONTAL = 3;
+    private static final int RIGHT_HORIZONTAL = 4;
+    private static final int TOP_VERTICAL = 5;
+    private static final int BOTTOM_VERTICAL = 6;
+    private int type;
+
+    public BoardLines (int type) {
+        super();
+        this.type = type;
+    }
+
+    @Override
+    public void paintComponent (Graphics g) {
+        switch(type) {
+            case(HORIZONTAL):
+                g.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
+                break;
+            case(VERTICAL):
+                g.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+                break;
+            case(LEFT_HORIZONTAL):
+                g.drawLine(getWidth() / 2, getHeight() / 2, getWidth(), getHeight() / 2);
+                break;
+            case(RIGHT_HORIZONTAL):
+                g.drawLine(0, getHeight() / 2, getWidth() / 2, getHeight() / 2);
+                break;
+            case(TOP_VERTICAL):
+                g.drawLine(getWidth() / 2, getHeight() / 2, getWidth() / 2, getHeight());
+                break;
+            case(BOTTOM_VERTICAL):
+                g.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight() / 2);
+                break;
+        }
+    }
 }
