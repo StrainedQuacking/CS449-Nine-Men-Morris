88690b1f02da1459713ee0887ddb517e4682c287
Sam Feye <samfeye@gmail.com>
2019-09-25 15:56:41 -0500

    Added gross drawBoardLines method to complete drawing the board.

--- NineMenMorris/src/main/java/projectmanagers/gui/panels/GamePanel.java
+++ NineMenMorris/src/main/java/projectmanagers/gui/panels/GamePanel.java
@@ -46,8 +46,38 @@ public class GamePanel extends JPanel {
     public void drawBoardLines() {
         for (int i = 0; i < 7 ; i ++) {
             for (int j = 0; j < 7; j++) {
-                if(Board.boardArray[i][j] == GameStatuses.ColorStatus.EMPTY){
+                if(Board.boardArray[i][j] == GameStatuses.ColorStatus.INVALID){
                     gbc.gridx = i; gbc.gridy = j;
+                    if ((j == 0 || j == 1 || j == 5 || j == 6) && (i != 0 && i != 6)) {
+                        gbc.fill = GridBagConstraints.HORIZONTAL;
+                        add(new BoardLines(2), gbc);
+                    }
+                    else if (j != 3 && i != 3){
+                        gbc.fill = GridBagConstraints.VERTICAL;
+                        add(new BoardLines(3), gbc);
+                    }
+                }
+                else {
+                    gbc.gridx = i; gbc.gridy = j;
+                    gbc.fill = GridBagConstraints.HORIZONTAL;
+                    if (((i == 1 || i == 5) && j == 3) || (i == 3 && j != 3))
+                        add(new BoardLines(2), gbc);
+                    else if ( (i == 0 || i == 1 || (i == 4 && j == 3) || (i == 2 && j != 3)))
+                        add(new BoardLines(1), gbc);
+                    else if ( (i == 5 || i == 6 || (i == 2 && j == 3) || (i == 4 && j != 3)))
+                        add(new BoardLines(5), gbc);
+                    gbc.fill = GridBagConstraints.VERTICAL;
+                    if ((j == 1 || j == 2) && i != 3)
+                        add(new BoardLines(0), gbc);
+                    else if (( j == 5 || j == 4) && i != 3)
+                        add(new BoardLines(6), gbc);
+                    else if ((j == 5 || j == 1) && i == 3)
+                        add(new BoardLines(3), gbc);
+                    else if (j == 4 && i == 3)
+                        add(new BoardLines(0), gbc);
+                    else if (j == 2 && i == 3)
+                        add(new BoardLines(6), gbc);
+                    add(new BoardLines(j), gbc);
                 }
             }
         }
