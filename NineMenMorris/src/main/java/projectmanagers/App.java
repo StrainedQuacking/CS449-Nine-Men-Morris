73129ff113a9e5414f24e3ff84876d19f08693d8
Sam Feye <samfeye@gmail.com>
2019-09-25 13:02:15 -0500

    Removed "Hello World"

--- NineMenMorris/src/main/java/projectmanagers/App.java
+++ NineMenMorris/src/main/java/projectmanagers/App.java
@@ -13,9 +13,6 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        System.out.println( "Hello World!");
-
         Board board = new Board();
         Position pos = new Position();
         Player W = new Player(GameStatuses.ColorStatus.WHITE);
