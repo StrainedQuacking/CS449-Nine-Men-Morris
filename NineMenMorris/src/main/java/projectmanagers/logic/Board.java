2ebd793d3eb6b48a6c2f85785a72c08b8934f5e2
Nathanael G <47373466+StrainedQuacking@users.noreply.github.com>
2019-10-23 21:23:22 -0500

    Actually refactored the board array

--- NineMenMorris/src/main/java/projectmanagers/logic/Board.java
+++ NineMenMorris/src/main/java/projectmanagers/logic/Board.java
@@ -1,9 +1,6 @@
 package main.java.projectmanagers.logic;
 
-
-
 import javafx.util.Pair;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -15,7 +12,7 @@ import static main.java.projectmanagers.trackers.PlayerTracking.PLAYER_LOOKUP;
 
 public class Board {
 
-    static private ColorStatus[][] boardArray = new ColorStatus[7][7];
+    static private List<List<Position>> boardArray = new ArrayList<>();
     static public ColorStatus[][] boardMills = new ColorStatus[16][3];
 
     public Board() {
@@ -24,49 +21,49 @@ public class Board {
 
     static void startingBoard() {
 
-        // xpos, ypos
-        boardArray[0][0] = EMPTY;
-        boardArray[0][3] = EMPTY;
-        boardArray[0][6] = EMPTY;
-
-        boardArray[1][1] = EMPTY;
-        boardArray[1][3] = EMPTY;
-        boardArray[1][5] = EMPTY;
-
-        boardArray[2][2] = EMPTY;
-        boardArray[2][3] = EMPTY;
-        boardArray[2][4] = EMPTY;
-
-        boardArray[3][0] = EMPTY;
-        boardArray[3][1] = EMPTY;
-        boardArray[3][2] = EMPTY;
-        boardArray[3][4] = EMPTY;
-        boardArray[3][5] = EMPTY;
-        boardArray[3][6] = EMPTY;
-
-        boardArray[4][2] = EMPTY;
-        boardArray[4][3] = EMPTY;
-        boardArray[4][4] = EMPTY;
-
-        boardArray[5][1] = EMPTY;
-        boardArray[5][3] = EMPTY;
-        boardArray[5][5] = EMPTY;
-
-        boardArray[6][0] = EMPTY;
-        boardArray[6][3] = EMPTY;
-        boardArray[6][6] = EMPTY;
-
-        for (int xpos = 0; xpos < 7; xpos++) {
-            for (int ypos = 0; ypos < 7; ypos++) {
-                if (boardArray[xpos][ypos] != EMPTY)
-                    boardArray[xpos][ypos] = INVALID;
+        for (int i = 0; i < 7; i++){
+            boardArray.add(new ArrayList<>());
+            for (int j = 0; j < 7; j++){
+                boardArray.get(i).add(new Position());
             }
         }
+        // xpos, ypos
+        boardArray.get(0).get(0).setStatus(EMPTY);
+        boardArray.get(0).get(3).setStatus(EMPTY);
+        boardArray.get(0).get(6).setStatus(EMPTY);
+
+        boardArray.get(1).get(1).setStatus(EMPTY);
+        boardArray.get(1).get(3).setStatus(EMPTY);
+        boardArray.get(1).get(5).setStatus(EMPTY);
+
+        boardArray.get(2).get(2).setStatus(EMPTY);
+        boardArray.get(2).get(3).setStatus(EMPTY);
+        boardArray.get(2).get(4).setStatus(EMPTY);
+
+        boardArray.get(3).get(0).setStatus(EMPTY);
+        boardArray.get(3).get(1).setStatus(EMPTY);
+        boardArray.get(3).get(2).setStatus(EMPTY);
+        boardArray.get(3).get(4).setStatus(EMPTY);
+        boardArray.get(3).get(5).setStatus(EMPTY);
+        boardArray.get(3).get(6).setStatus(EMPTY);
+
+        boardArray.get(4).get(2).setStatus(EMPTY);
+        boardArray.get(4).get(3).setStatus(EMPTY);
+        boardArray.get(4).get(4).setStatus(EMPTY);
+
+        boardArray.get(5).get(1).setStatus(EMPTY);
+        boardArray.get(5).get(3).setStatus(EMPTY);
+        boardArray.get(5).get(5).setStatus(EMPTY);
+
+        boardArray.get(6).get(0).setStatus(EMPTY);
+        boardArray.get(6).get(3).setStatus(EMPTY);
+        boardArray.get(6).get(6).setStatus(EMPTY);
+
     }
 
     static public boolean placePiece(Player player, int xpos, int ypos) {
-        if ((Board.boardArray[xpos][ypos] == EMPTY) && (Board.boardArray[xpos][ypos] != INVALID)) {
-            Board.boardArray[xpos][ypos] = player.getColor();
+        if ((Board.boardArray.get(xpos).get(ypos).getStatus() == EMPTY) && (Board.boardArray.get(xpos).get(ypos).getStatus() != INVALID)) {
+            Board.boardArray.get(xpos).get(ypos).setStatus(player.getColor());
             player.incrementPieces();
             determineMills();
             return true;
@@ -76,9 +73,9 @@ public class Board {
     }
 
     static public boolean remove(int xpos, int ypos) {
-        if (boardArray[xpos][ypos] != EMPTY && boardArray[xpos][ypos] != INVALID) {
-            PLAYER_LOOKUP.get(boardArray[xpos][ypos]).decrementPieces();
-            boardArray[xpos][ypos] = EMPTY;
+        if (boardArray.get(xpos).get(ypos).getStatus() != EMPTY && boardArray.get(xpos).get(ypos).getStatus() != INVALID) {
+            PLAYER_LOOKUP.get(boardArray.get(xpos).get(ypos).getStatus()).decrementPieces();
+            boardArray.get(xpos).get(ypos).setStatus(EMPTY);
             return true;
         } else {
             return false;
@@ -86,7 +83,11 @@ public class Board {
     }
 
     static public ColorStatus position(int xpos, int ypos) {
-        return boardArray[xpos][ypos];
+        return boardArray.get(xpos).get(ypos).getStatus();
+    }
+
+    static public boolean isPositionMilled(int xpos, int ypos) {
+        return boardArray.get(xpos).get(ypos).isMilled();
     }
 
     static private void determineMills() {
@@ -302,6 +303,4 @@ public class Board {
         }
         return adjacentPieces;
     }
-
-
 }
--- NineMenMorris/src/main/java/projectmanagers/logic/Position.java
+++ NineMenMorris/src/main/java/projectmanagers/logic/Position.java
@@ -1,4 +1,31 @@
 package main.java.projectmanagers.logic;
 
+import main.java.projectmanagers.logic.GameStatuses.ColorStatus;
+
+import static main.java.projectmanagers.logic.GameStatuses.ColorStatus.INVALID;
+
 public class Position {
+    private ColorStatus colorStatus;
+    private boolean millStatus;
+    Position() {
+        this.colorStatus = INVALID;
+        this.millStatus = false;
+    }
+
+    Position(ColorStatus colorStatus) {
+        this.colorStatus = colorStatus;
+        this.millStatus = false;
+    }
+
+    ColorStatus getStatus() {
+        return colorStatus;
+    }
+
+    void setStatus(ColorStatus colorStatus) {
+        this.colorStatus = colorStatus;
+    }
+
+    boolean isMilled() {
+        return millStatus;
+    }
 }
