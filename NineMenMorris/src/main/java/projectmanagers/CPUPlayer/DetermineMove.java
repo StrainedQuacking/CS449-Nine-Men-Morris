fd5f47f2976885321a5e54f804b7b3310904173d
Nathanael G <47373466+StrainedQuacking@users.noreply.github.com>
2019-11-20 19:56:34 -0600

    holy frick it might actually work

--- NineMenMorris/src/main/java/projectmanagers/CPUPlayer/DetermineMove.java
+++ NineMenMorris/src/main/java/projectmanagers/CPUPlayer/DetermineMove.java
@@ -9,6 +9,7 @@ import java.util.Random;
 
 import static main.java.projectmanagers.logic.GameStatuses.ColorStatus;
 import static main.java.projectmanagers.logic.GameStatuses.ColorStatus.BLUE;
+import static main.java.projectmanagers.logic.GameStatuses.ColorStatus.EMPTY;
 import static main.java.projectmanagers.logic.GameStatuses.NO_PLACE;
 
 public class DetermineMove {
@@ -36,6 +37,7 @@ public class DetermineMove {
 
     static Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> movementMills(ColorStatus color) {
         List<Pair<Integer, Integer>> positions = Board.getEmptyPieces();
+        Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> result = new Pair<>(NO_PLACE, NO_PLACE);
 
         for (Pair<Integer, Integer> position : positions) {
             Pair<ColorStatus, List<Pair<Integer, Integer>>> millInfo = Board.isPositionCloseToMilled(position);
@@ -46,11 +48,17 @@ public class DetermineMove {
                 for (Pair<Integer, Integer> adjacent : adjacents) {
                     if (Board.position(adjacent).equals(BLUE) && !millInfo.getValue().contains(adjacent)) {
                         return new Pair<>(adjacent, position);
+                    } else if (Board.position(adjacent).equals(EMPTY)){
+                        for (Pair<Integer, Integer> pair : Board.adjacentPieces(adjacent.getKey(), adjacent.getValue())){
+                            if (Board.position(pair).equals(BLUE)) {
+                                result = new Pair<>(pair, adjacent);
+                            }
+                        }
                     }
                 }
             }
         }
-        return new Pair<>(NO_PLACE, NO_PLACE);
+        return result;
     }
 
 }
